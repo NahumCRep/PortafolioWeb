13f9a59212e3140955ca3868fb29abc28bc4187c
Nahum Casco <nahumcasco.v@gmail.com>
2022-03-01 14:53:56 -0500

    new path filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/public/static/images`,
+        path: `${__dirname}/static/images`,
       },
     },
     `gatsby-transformer-sharp`,
