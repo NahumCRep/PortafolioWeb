6187e5e95ea8a0f3971cd708e9df91db87540aeb
Nahum Casco <nahumcasco.v@gmail.com>
2022-03-14 16:33:16 -0500

    cleaning inputs on success

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -76,9 +76,9 @@ const Contact = () => {
                 <form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field" onSubmit={e => sendEmail(e)} >
                     <input type="hidden" name="form-name" value="contact" />
                     <h3>{t("contact.cctitle")}</h3>
-                    <input type="text" aria-label='name' name='name' placeholder={t("contact.phname")} onChange={handleChange} required />
-                    <input type="email" aria-label='email' name='email' placeholder={t("contact.phemail")} onChange={handleChange} required />
-                    <textarea name='message' onChange={handleChange} required></textarea>
+                    <input type="text" aria-label='name' name='name' placeholder={t("contact.phname")} onChange={handleChange} value={formData.name} required />
+                    <input type="email" aria-label='email' name='email' placeholder={t("contact.phemail")} onChange={handleChange} value={formData.email} required />
+                    <textarea name='message' onChange={handleChange} value={formData.message} required></textarea>
                     <div className="form-btns">
                         <button type="submit">
                             {
