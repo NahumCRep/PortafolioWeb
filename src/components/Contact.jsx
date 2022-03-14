09eabc7d528932229291347ef6dd259b9b9641b5
Nahum Casco <nahumcasco.v@gmail.com>
2022-03-14 13:32:14 -0500

    contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../css/contact.css';
 import { useTranslation } from 'gatsby-plugin-react-i18next';
+import axios from "axios"
+import * as qs from "query-string"
 
 const Contact = () => {
-    const {t} = useTranslation();
+    const { t } = useTranslation();
+    const [formData, setFormData] = useState({
+        name:'',
+        email:'',
+        message:''
+    })
 
-    return(
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+        
+    }
+
+    return (
         <section id="section-contact" className="Contact-Container">
             <div className="Cct-form">
-                <form>
+                <form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field" onSubmit={e => sendEmail(e)} >
+                    <input type="hidden" name="form-name" value="contact" />
                     <h3>{t("contact.cctitle")}</h3>
-                    <input type="text" aria-label='name' placeholder={t("contact.phname")} required/>
-                    <input type="email" aria-label='email' placeholder={t("contact.phemail")} required/>
-                    <textarea required></textarea>
+                    <input type="text" aria-label='name' name='name' placeholder={t("contact.phname")} onChange={handleChange} required />
+                    <input type="email" aria-label='email' name='email' placeholder={t("contact.phemail")} onChange={handleChange} required />
+                    <textarea name='message' onChange={handleChange} required></textarea>
                     <div className="form-btns">
                         <button type="submit"
-                            onSubmit={(e)=>{
-                                e.preventDefault();
-                                alert("hola");
-                            }}
                         >{t("contact.btnsend")}</button>
                     </div>
                 </form>
